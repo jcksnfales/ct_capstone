649bfb8810b3708e06b2ed15e72d8e58cfd8ed04
jcksnfales <jcksnfales@gmail.com>
2023-06-22 19:12:49 -0400

    Copied auth functionality from book catalog webapp

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'SUPER DUPER SECRET KEY'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
