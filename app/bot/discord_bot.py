43928a0878fb9bbfabd1306a7e61288b9bad37fa
jcksnfales <jcksnfales@gmail.com>
2023-07-04 19:00:08 -0400

    Added /links_share_all command to bot

--- app/bot/discord_bot.py
+++ app/bot/discord_bot.py
@@ -3,15 +3,35 @@ from models import db, User, LinkListing, link_schema, links_schema
 
 discord_bot = DiscordInteractionsBlueprint()
 
-@discord_bot.command("hello")
-def hello(ctx):
-    "Simple hello world test command"
-    return "Hello world!"
+# @discord_bot.command("hello")
+# def hello(ctx):
+#     "Simple hello world test command"
+#     return "Hello world!"
 
-@discord_bot.command("share_user_links")
-def share_user_links(ctx, user_id: str):
+@discord_bot.command("links_share_all")
+def links_share_all(ctx):
+    "Returns all publicly listed links."
+    listings = LinkListing.query.filter_by(is_public=True).all()
+    links_component = []
+    for listing in listings:
+        links_component += [
+            ActionRow(components=[
+                    Button(
+                        style=ButtonStyles.LINK,
+                        url=listing.listed_link,
+                        label=listing.link_title
+                    )
+            ])
+        ]
+    return Message(
+        content=f"All public links",
+        components=links_component
+    )
+
+@discord_bot.command("links_share_user")
+def links_share_user(ctx, user_id: str):
     "Returns a given user's publicly listed links"
-    listings = LinkListing.query.filter_by(user_id=user_id).all()
+    listings = LinkListing.query.filter_by(user_id=user_id, is_public=True).all()
     user = User.query.get(user_id)
     links_component = []
     for listing in listings:
@@ -23,8 +43,7 @@ def share_user_links(ctx, user_id: str):
                         url=listing.listed_link,
                         label=listing.link_title
                     )
-                ]
-            )
+            ])
         ]
     return Message(
         content=f"Links from **{user.email}**",
