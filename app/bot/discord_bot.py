e0d12c5cacb39192f789001446ffc05eea110ccc
jcksnfales <jcksnfales@gmail.com>
2023-07-04 18:12:48 -0400

    Updated share_user_links command output formatting

--- app/bot/discord_bot.py
+++ app/bot/discord_bot.py
@@ -1,4 +1,4 @@
-from flask_discord_interactions import DiscordInteractionsBlueprint
+from flask_discord_interactions import DiscordInteractionsBlueprint, Message, ActionRow, Button, ButtonStyles
 from models import db, User, LinkListing, link_schema, links_schema
 
 discord_bot = DiscordInteractionsBlueprint()
@@ -11,7 +11,25 @@ def hello(ctx):
 @discord_bot.command("share_user_links")
 def share_user_links(ctx, user_id: str):
     "Returns a given link's information"
-    return LinkListing.query.filter_by(user_id=user_id).all()
+    listings = LinkListing.query.filter_by(user_id=user_id).all()
+    links_component = []
+    for listing in listings:
+        links_component += [
+            ActionRow(
+                content=listing.link_title,
+                components=[
+                    Button(
+                        style=ButtonStyles.LINK,
+                        url=listing.listed_link,
+                        label="Go to link"
+                    )
+                ]
+            )
+        ]
+    return Message(
+        content=f"Links from **{user_id.email}**",
+        components=links_component
+    )
 
 @share_user_links.autocomplete()
 def autocomplete(ctx, user_id=None):
