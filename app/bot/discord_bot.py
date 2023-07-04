75ea72bbfad952deae8d7184142a8e7da56d2d8f
jcksnfales <jcksnfales@gmail.com>
2023-07-04 17:47:27 -0400

    Update discord_bot.py

--- app/bot/discord_bot.py
+++ app/bot/discord_bot.py
@@ -3,17 +3,17 @@ from models import db, User, LinkListing, link_schema, links_schema
 
 discord_bot = DiscordInteractionsBlueprint()
 
-@discord_bot.command()
+@discord_bot.command("hello")
 def hello(ctx):
     "Simple hello world test command"
     return "Hello world!"
 
-@discord_bot.command()
+@discord_bot.command("share_link")
 def share_link(ctx, value: str):
     "Returns a given link's information"
     return f"{value}"
 
-@discord_bot.autocomplete()
+@share_link.autocomplete()
 def autocomplete(ctx, value=None):
     queried_links = LinkListing.query.filter_by(is_public=True).all()
     return queried_links
