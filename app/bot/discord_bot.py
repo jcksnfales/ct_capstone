fddee119c499a2a9831369ea599aeb7f0c700f1a
jcksnfales <jcksnfales@gmail.com>
2023-07-04 17:12:58 -0400

    Update discord_bot.py

--- app/bot/discord_bot.py
+++ app/bot/discord_bot.py
@@ -1,6 +1,6 @@
 from flask_discord_interactions import DiscordInteractionsBlueprint
 
-discord_bot = DiscordInteractionsBlueprint
+discord_bot = DiscordInteractionsBlueprint()
 
 @discord_bot.command()
 def hello(ctx):
